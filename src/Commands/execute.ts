fb6feb6ce8321088cae309453fdc8aa3345bae30
m-hayabusa <s.hashimoto@hs-sh.net>
2020-12-29 17:33:03 +0900

    execute: APIクラスを触れるようにした

--- src/Commands/execute.ts
+++ src/Commands/execute.ts
@@ -1,14 +1,16 @@
 import command from "./command";
+import API from "../Connection/API"
 
 export default class execute extends command {
     regex = /^(f)/;
-    help = "execute:\t> f <arg>\t-> Function(arg)()";
+    help = "execute:\t> f <arg>\t-> eval(arg) // api.request() で当該ユーザーの権限でMisskey APIにアクセス可能";
     function = function(arg:string){
         if (!arg) {
             return (new execute).help;
         }
         try {
-            return "execute: " + Function(arg)();
+            let api = new API();
+            return "execute: " + eval(arg);
         } catch (e) {
             return "execute: " + e;
         }
