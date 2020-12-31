b5b46b4b60258fa0d67184ac2638a2cb03aa46da
m-hayabusa <s.hashimoto@hs-sh.net>
2020-12-31 22:23:43 +0900

    Streaming: タイムラインの表示をいい感じに

--- src/Commands/connectStream.ts
+++ src/Commands/connectStream.ts
@@ -12,8 +12,28 @@ export default class createNote extends command {
         createNote.Id = stream.connectChannel(new StreamingBody("homeTimeline"), (data:any)=>{
             // console.log(data);
             if (data.type === "note"){
-                console.log(data.body.user.name, data.body.user.username, data.body.user.host)
-                console.log(data.body.text)
+                // input.prompt(false);
+                process.stdout.write("\x1b[2K");
+
+                if (data.body.renote) {
+                    console.log("\x1b[G" + "\x1b[46m " + data.body.user.name + " \x1b[43m Re "
+                                                 + "\x1b[42m " + data.body.renote.user.name + " \x1b[0m");
+                } else {
+                    console.log("\x1b[G" + "\x1b[46m " + data.body.user.name + " \x1b[0m");
+                }
+
+                process.stdout.write(data.body.text ? data.body.text+'\n':'')
+                
+                if (data.body.renote) {
+                    console.log("> " , data.body.renote.text)
+                }
+
+                let id = data.body.user.username + (data.body.user.host ? ('@' + data.body.user.host):'');
+                if (data.body.renote){
+                    id += ' Re '+data.body.renote.user.username + '@' + data.body.renote.user.host;
+                }
+                console.log("\x1b[G\x1b[47m\x1b[30m " + id + ' '.repeat(process.stdout.columns - data.body.createdAt.length - id.toString().length -2) + data.body.createdAt + " \x1b[0m")
+                
                 input.prompt(true);
             }
         });
